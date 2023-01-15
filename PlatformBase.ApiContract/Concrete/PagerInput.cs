49555512d4b6880c62f4b37b37a2df9a186bd211
Ertuğrul Güler <ertugrul.guler@outlook.com.tr>
2023-01-15 15:48:54 +0300

    domain projesi eklendi çeşitli güncellemeler yapıldı

---
+++ PlatformBase.ApiContract/Concrete/PagerInput.cs
@@ -0,0 +1,24 @@
+﻿namespace PlatformBase.Domain.Concrete;
+
+public class PagerInput
+{
+    public int PageIndex { get; set; }
+
+    public int PageSize { get; set; }
+
+    public PagerInput(int pageIndex, int pageSize)
+    {
+        if (pageIndex == 0)
+        {
+            pageIndex = 1;
+        }
+
+        if (pageSize == 0)
+        {
+            pageSize = 10;
+        }
+
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+    }
+}
---
+++ PlatformBase.Repository/DbContextHandler.cs
@@ -0,0 +1,30 @@
+﻿using PlatformBase.Domain;
+
+namespace PlatformBase.Repository;
+
+//public class DbContextHandler : IDbContextHandler
+//{
+//    private readonly CatalogDbContext _dbContext;
+//    private readonly IAppLogger _appLogger;
+
+//    public DbContextHandler(CatalogDbContext dbContext, IAppLogger appLogger)
+//    {
+//        _dbContext = dbContext;
+//        _appLogger = appLogger;
+//    }
+
+
+//    public async Task SaveChangesAsync()
+//    {
+//        try
+//        {
+//            await _dbContext.SaveChangesAsync();
+//        }
+//        catch (Exception ex)
+//        {
+//            _appLogger.Exception(ex, null);
+//            _dbContext.ChangeTracker.Clear();
+//            throw ex;
+//        }
+//    }
+//}
